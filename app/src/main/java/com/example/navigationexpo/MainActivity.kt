d7033c2eeeb7e922305eecbc6d9336d161141578
Daniel Peñaloza <daniel.penaloza.sge@mango.com>
2024-04-22 22:50:00 +0200

    Voyager implementation

--- app/src/main/java/com/example/navigationexpo/MainActivity.kt
+++ app/src/main/java/com/example/navigationexpo/MainActivity.kt
@@ -12,102 +12,124 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import com.example.navigationexpo.ui.theme.NavigationExpoTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NavigationExpoTheme {
+                Navigator(screen = MainScreen())
             }
         }
     }
 }
 
-@Composable
-fun MainScreen(
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+class MainScreen(
+    private val modifier: Modifier = Modifier
+) : Screen {
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+
+        Surface(
+            modifier = modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
         ) {
-            Text(
-                text = "Pantalla Principal",
-                fontSize = 28.sp
-            )
-            Row {
-                Button(onClick = { }) {
-                    Text(text = "Pasar un String")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Pantalla Principal",
+                    fontSize = 28.sp
+                )
+                Row {
+                    Button(onClick = { navigator.push(StringScreen("Mango")) }) {
+                        Text(text = "Pasar un String")
+                    }
+                    Button(onClick = { navigator.push(IntScreen(1234)) }) {
+                        Text(text = "Pasar un Int")
+                    }
                 }
-                Button(onClick = { }) {
-                    Text(text = "Pasar un Int")
+                Button(onClick = { navigator.push(ObjectScreen(CustomObject(2024, "Abril"))) }) {
+                    Text(text = "Pasar un Objeto")
+                }
+                Button(onClick = { navigator.push(SecondScreen()) }) {
+                    Text(text = "Ir a una siguiente pantalla")
                 }
             }
-            Button(onClick = { }) {
-                Text(text = "Pasar un Objeto")
-            }
-            Button(onClick = { }) {
-                Text(text = "Ir a una siguiente pantalla")
+        }
+    }
+}
+
+data class StringScreen(
+    val name: String,
+    val modifier: Modifier = Modifier
+) : Screen {
+
+    @Composable
+    override fun Content() {
+        Surface(
+            modifier = modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Pantalla con String",
+                    fontSize = 28.sp
+                )
+                Text(
+                    text = name,
+                    fontSize = 24.sp
+                )
             }
         }
     }
 }
 
-@Composable
-fun StringScreen(
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Pantalla con String",
-                fontSize = 28.sp
-            )
-            Text(
-                text = name,
-                fontSize = 24.sp
-            )
-        }
-    }
-}
 
+data class IntScreen(
+    val number: Int,
+    val modifier: Modifier = Modifier
+) : Screen {
 
-@Composable
-fun IntScreen(
-    number: Int,
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    @Composable
+    override fun Content() {
+        Surface(
+            modifier = modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
         ) {
-            Text(
-                text = "Pantalla con Int",
-                fontSize = 28.sp
-            )
-            Text(
-                text = number.toString(),
-                fontSize = 24.sp
-            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Pantalla con Int",
+                    fontSize = 28.sp
+                )
+                Text(
+                    text = number.toString(),
+                    fontSize = 24.sp
+                )
+            }
         }
     }
 }
@@ -117,76 +139,108 @@ data class CustomObject(
     val name: String
 )
 
-@Composable
-fun ObjectScreen(
-    customObject: CustomObject,
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Pantalla con Object",
-                fontSize = 28.sp
-            )
-            Text(
-                text = customObject.name,
-                fontSize = 24.sp
-            )
-            Text(
-                text = customObject.number.toString(),
-                fontSize = 24.sp
-            )
-        }
-    }
+class ObjectViewModel(
+    customObject: CustomObject
+) : ViewModel() {
+
+    private val _customObject: MutableStateFlow<CustomObject> =
+        MutableStateFlow(customObject)
+    val customObject = _customObject.asStateFlow()
+
+
 }
 
-@Composable
-fun SecondScreen(
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+data class ObjectScreen(
+    val customObject: CustomObject,
+    private val modifier: Modifier = Modifier
+) : Screen {
+
+    @Composable
+    override fun Content() {
+
+        val screenModel = viewModel<ObjectViewModel>(factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return ObjectViewModel(customObject) as T
+            }
+        })
+
+        val vmCustomObject = screenModel.customObject.collectAsState()
+
+        Surface(
+            modifier = modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
         ) {
-            Text(
-                text = "Segunda pantalla",
-                fontSize = 28.sp
-            )
-            Button(onClick = { }) {
-                Text(text = "Ir a la tercera pantalla")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Pantalla con Object",
+                    fontSize = 28.sp
+                )
+                Text(
+                    text = vmCustomObject.value.name,
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = vmCustomObject.value.number.toString(),
+                    fontSize = 24.sp
+                )
             }
         }
     }
 }
 
-@Composable
-fun ThirdScreen(
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+class SecondScreen(
+    private val modifier: Modifier = Modifier
+) : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+
+        Surface(
+            modifier = modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
         ) {
-            Text(
-                text = "Tercera pantalla",
-                fontSize = 28.sp
-            )
-            Button(onClick = { }) {
-                Text(text = "Regresar al Inicio")
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Segunda pantalla",
+                    fontSize = 28.sp
+                )
+                Button(onClick = { navigator.push(ThirdScreen()) }) {
+                    Text(text = "Ir a la tercera pantalla")
+                }
+            }
+        }
+    }
+}
+
+class ThirdScreen(
+    val modifier: Modifier = Modifier
+) : Screen {
+
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+
+        Surface(
+            modifier = modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Tercera pantalla",
+                    fontSize = 28.sp
+                )
+                Button(onClick = { navigator.popUntil { it == MainScreen::key } }) {
+                    Text(text = "Regresar al Inicio")
+                }
             }
         }
     }
